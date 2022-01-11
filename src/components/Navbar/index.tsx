45cdd8c26a4c602366f036c319454651d4ab1dc0
Leonardo Vargas <leu1607@hotmail.com>
2022-01-10 21:36:54 -0300

    feat: adicionando responsividade ao app

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,25 +1,45 @@
+import { useState } from 'react'
 import styles from './styles.module.scss'
 
-export const Navbar = () => (
-  <div className={styles.container}>
-    <h1>SaFe Travel.</h1>
+export const Navbar = () => {
+  const [activeNavigationMobile, setActiveNavigationMobile] = useState(false)
 
-    <nav className={styles.navigation}>
-      <ul>
-        <li>Discover</li>
-        <li>Special Deals</li>
-        <li>Community</li>
-        <li>About us</li>
-      </ul>
+  return (
+    <div className={styles.container}>
+      <h1>SaFe Travel.</h1>
 
-      <div className={styles['content-buttons']}>
-        <button type="button" className={styles['button-sign-in']}>
-          Sign In
-        </button>
-        <button type="button" className={styles['button-register']}>
-          Register
-        </button>
-      </div>
-    </nav>
-  </div>
-)
+      <nav
+        className={`${styles.navigation} ${
+          activeNavigationMobile ? styles.active : ''
+        }`}
+      >
+        <ul>
+          <li>Discover</li>
+          <li>Special Deals</li>
+          <li>Community</li>
+          <li>About us</li>
+        </ul>
+
+        <div className={styles['content-buttons']}>
+          <button type="button" className={styles['button-sign-in']}>
+            Sign In
+          </button>
+          <button type="button" className={styles['button-register']}>
+            Register
+          </button>
+        </div>
+      </nav>
+
+      <button
+        type="button"
+        className={`${styles['button-mobile']} ${
+          activeNavigationMobile ? styles['button-active'] : ''
+        }`}
+        onClick={() => setActiveNavigationMobile(state => !state)}
+      >
+        <span />
+        <span />
+      </button>
+    </div>
+  )
+}
